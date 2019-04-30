6cd831a3f21219ff4d59b97db42335a80c01892d
njanirudh <anijaya9@gmail.com>
2019-04-30 21:09:38 +0200

    Corrected graphing code

--- Assignment 2/graphing.py
+++ Assignment 2/graphing.py
@@ -5,6 +5,8 @@ import cv2 as cv2
 import json
 import ast
 import math
+from matplotlib.ticker import MultipleLocator
+
 
 def text_reader(path):
     """
@@ -49,9 +51,8 @@ def create_csv(path):
 def drawArrow(A, B):
     #plt.arrow(A[0], A[1], B[0] - A[0], B[1] - A[1],
     #          length_includes_head=False, head_width=0.05, head_length=0.1, fc='k', ec='k')
-    plt.arrow(A[0], A[1], B[0] - A[0], B[1] - A[1],
-              head_width=3, length_includes_head=True, head_length=3.0,
-              fc='red', ec='black',width = 0.01)
+    plt.arrow(A[0], A[1], B[0] - A[0], B[1] - A[1], head_width=3, length_includes_head=True, head_length=4.0,
+                  fc='red', ec='black')
 
 if __name__ == "__main__":
 
@@ -78,9 +79,9 @@ if __name__ == "__main__":
     #create_csv("Text/left.txt")
 
     #img = cv2.imread("/home/anirudh/Desktop/SEE/SEE-Project/Assignment 2/Results_1/Right/IMG_20190413_233532.jpg")
-    #img = cv2.imread("/home/anirudh/Desktop/SEE/SEE-Project/Assignment 2/Results_1/Start/IMG_20190413_223849.jpg")
+    img = cv2.imread("/home/anirudh/Desktop/SEE/SEE-Project/Assignment 2/Results_1/Start/IMG_20190413_223849.jpg")
     #img = cv2.imread("/home/anirudh/Desktop/SEE/SEE-Project/Assignment 2/Results_1/Left/IMG_20190413_232209.jpg")
-    img = cv2.imread("/home/anirudh/Desktop/SEE/SEE-Project/Assignment 2/Results_1/Forward/IMG_20190413_225323.jpg")
+    #img = cv2.imread("/home/anirudh/Desktop/SEE/SEE-Project/Assignment 2/Results_1/Forward/IMG_20190413_225323.jpg")
 
     # Setting the axis size according to the sheet size of the map
     # in which the robot is run
@@ -88,8 +89,27 @@ if __name__ == "__main__":
     axes.set_xlim([0, 952.5 *SCALE_FACTOR])
     axes.set_ylim([0, 685.8 *SCALE_FACTOR])
 
+    # I want minor ticks to be every 5
+    major_ticks_x = np.arange(0, 952.5, 100)
+    major_ticks_y = np.arange(0, 685.8, 100)
+    minor_ticks_x = np.arange(0, 952.5, 20)
+    minor_ticks_y = np.arange(0, 685.8, 20)
+
+    axes.set_xticks(major_ticks_x)
+    axes.set_yticks(major_ticks_y)
+    axes.set_xticks(minor_ticks_x, minor=True)
+    axes.set_yticks(minor_ticks_y, minor=True)
+
+    # Specify tick label size
+    axes.tick_params(axis='both', which='major', labelsize=5)
+    axes.tick_params(axis='both', which='minor', labelsize=0)
+
+    axes.grid(which='minor', alpha=0.2)
+    axes.grid(which='major', alpha=0.4)
+
+
     # Showing the BG image in the plot
-    plt.imshow(img, zorder=0, extent=[0, 952.5*SCALE_FACTOR, 685.8*SCALE_FACTOR, 0])
+    #plt.imshow(img, zorder=0, extent=[0, 952.5*SCALE_FACTOR, 685.8*SCALE_FACTOR, 0])
     #plt.imshow(img, zorder=0, extent=[0,  685.8 ,952.5, 0])
 
     # Draw scatter plot of the (x,y) coordinates
@@ -109,25 +129,26 @@ if __name__ == "__main__":
     for i in range(len(forward_front_marker[0])-1):
         drawArrow([forward_back_marker[0][i], forward_back_marker[1][i]],
               [forward_front_marker[0][i],forward_front_marker[1][i]])
-    #
-    # for i in range(len(right_front_marker[0])-1):
-    #     drawArrow([right_back_marker[0][i], right_back_marker[1][i]],
-    #           [right_front_marker[0][i],right_front_marker[1][i]])
 
-    # for i in range(len(left_front_marker[0])-1):
-    #     drawArrow([left_back_marker[0][i], left_back_marker[1][i]],
-    #           [left_front_marker[0][i],left_front_marker[1][i]])
+    for i in range(len(right_front_marker[0])-1):
+        drawArrow([right_back_marker[0][i], right_back_marker[1][i]],
+              [right_front_marker[0][i],right_front_marker[1][i]])
 
-    # for i in range(len(start_front_marker[0])-1):
-    #     drawArrow([start_back_marker[0][i], start_back_marker[1][i]],
-    #           [start_front_marker[0][i],start_front_marker[1][i]])
+    for i in range(len(left_front_marker[0])-1):
+        drawArrow([left_back_marker[0][i], left_back_marker[1][i]],
+              [left_front_marker[0][i],left_front_marker[1][i]])
+
+    for i in range(len(start_front_marker[0])-1):
+        drawArrow([start_back_marker[0][i], start_back_marker[1][i]],
+              [start_front_marker[0][i],start_front_marker[1][i]])
 
 
-    plt.title('End pose - Straight')
+    plt.title('All poses')
     plt.xlabel('x (mm)')
     plt.ylabel('y (mm)')
 
-    plt.savefig('straight.png', dpi=1000)
+
+    plt.savefig('all_wo_bg.png', dpi=1000)
 
     plt.show()
 
